d6bf891ea8d92c9107fe4b3c5fb166dca8550246
testbotvanalex <testbotvanalex@gmail.com>
2025-08-16 21:49:52 +0200

    dada

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
-echo "export default function Home() {
+export default function Home() {
   return (
-    <div style={{ fontFamily: 'sans-serif', textAlign: 'center', padding: '2rem' }}>
-      <h1>🚀 BotMatic</h1>
-      <p>Welkom bij je nieuwe Next.js site!</p>
-    </div>
-  )
-}" > pages/index.js
+    <main style={{ fontFamily: 'sans-serif', padding: '2rem' }}>
+      <h1>🚀 Привет, мир!</h1>
+      <p>Это тестовая сборка на Vercel без ошибок.</p>
+    </main>
+  );
+}
