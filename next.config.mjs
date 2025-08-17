0a780c99aafa2c319bcb9efe78282ec89db79cc5
Alexandre Stoliarov <__email@.com>
2025-08-17 16:58:45 +0200

    fix: next config (remove experimental.appDir)

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  experimental: { appDir: true }
-};
+const nextConfig = { reactStrictMode: true };
 export default nextConfig;
